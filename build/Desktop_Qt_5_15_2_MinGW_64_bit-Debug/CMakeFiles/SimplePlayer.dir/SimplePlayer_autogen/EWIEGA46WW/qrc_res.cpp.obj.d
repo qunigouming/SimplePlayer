01ded7a8e7f1a7d5ea04d33babf1ad74d9c6495f
qunigoumin <3120248848@qq.com>
2025-09-17 14:12:20 +0800

    delete build folder

--- build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/CMakeFiles/SimplePlayer.dir/SimplePlayer_autogen/EWIEGA46WW/qrc_res.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/SimplePlayer.dir/SimplePlayer_autogen/EWIEGA46WW/qrc_res.cpp.obj: \
- E:\CppProject\SimplePlayer\build\Desktop_Qt_5_15_2_MinGW_64_bit-Debug\SimplePlayer_autogen\EWIEGA46WW\qrc_res.cpp
--- build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/CMakeFiles/SimplePlayer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SimplePlayer.
-# This may be replaced when dependencies are built.
